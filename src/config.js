b493eeda931b1bc2eea1ebfdd7b5e8a6207312a1
esixtosr <esixtosr@proton.me>
2026-08-20 01:24:16 -0400

    Update social media links

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/esixtosr',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/flaco0_0/',
     },
     {
       name: 'Linkedin',
